6ef0347abc4f11209901d5bfbf0eb7416d87d18b
YourGodzilla <theo.yuriputra19@gmail.com>
2025-01-10 14:07:16 +0700

    #

--- Lab/struct.cpp
+++ Lab/struct.cpp
@@ -4,10 +4,10 @@
 struct Student {
     int age;
     int height;
-};
+}arrStudent[5];
 
 int main () {
-    struct Student student;
+    Student student;
 
     // akses anggota struct5
     student.age = 20;
@@ -15,4 +15,17 @@ int main () {
 
     // Alokasi memori untuk struct
     struct Student* ptr2  = (Student*)malloc(sizeof(struct Student));
+
+    // Akses anggota dari struct pointer
+    ptr2->age = 12;
+    ptr2->height = 175;
+
+    printf("Age: %d, height: %d", ptr2->age, ptr2->height);
+
+    // melepaskan alokasi memori
+    free(ptr2);
+
+    // akses anggota dari array of struct
+    arrStudent[0].age = 12;
+    arrStudent[1].age = 13;
 }
