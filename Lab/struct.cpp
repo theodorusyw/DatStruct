37d7855a0d568683ceacdc6026ae01e479aa53a2
YourGodzilla <theo.yuriputra19@gmail.com>
2025-01-10 13:47:57 +0700

    #

---
+++ Lab/struct.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    int age;
+    int height;
+};
+
+int main () {
+    struct Student student;
+
+    student.age = 20;
+    student.height = 170;
+
+    // Alokasi memori untuk struct
+    struct Student* ptr2  = (Student*)malloc(sizeof(struct Student));
+}
