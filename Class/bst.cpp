46005bf380e46ac5cef5c05521a92cc9e262426a
YourGodzilla <theo.yuriputra19@gmail.com>
2025-02-07 13:35:17 +0700

    #

---
+++ Class/bst.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct Student {
+    int id;
+    char name[101];
+    struct Student *left, *right;
+};
+
+struct Student *createStudent(int id, char *name) {
+    struct Student *newStudent = (struct Student*)malloc(sizeof(Student));
+    newStudent->id = id;
+    strcpy(newStudent->name, name);
+    newStudent->left = newStudent->right = NULL;
+    return newStudent;
+}
+
+struct Student *insert(struct Student *root, int id, char *name) {
+    // Case 1: kalo tree blm dibuat, kita bikin root nodenya
+    if (root == NULL) {
+        return createStudent(id, name);
+    }
+
+    // Case 2: ID yang kita mau insert > current node punya ID
+    else if (id > root->id) {
+        root->right = insert(root->right, id, name);
+    }
+    
+    // Case 3: ID yang mau kita insert < current node punya ID
+    else if (id < root->id) {
+        root->left = insert(root->left, id, name);
+    }
+
+    return root;
+}
