0c808cb881e0b53f2101596ed0f8684c63ecfe09
YourGodzilla <theo.yuriputra19@gmail.com>
2025-02-07 14:18:57 +0700

    #

--- Class/bst.cpp
+++ Class/bst.cpp
@@ -32,6 +32,8 @@ struct Student *insert(struct Student *root, int id, char *name) {
         root->left = insert(root->left, id, name);
     }
 
+    // Case 4: kalo valuenya udah ada, kita return nodenya saja
+
     return root;
 }
 
@@ -40,6 +42,30 @@ void inorder(struct Student *root) {
     inorder(root->left);
     printf("Student ID: %d, Name: %s\n", root->id, root->name);
     inorder(root->right);
+}
+
+struct Student *search(struct Student *root ,int id) {
+    // Case 1: kalo Search ga ketemu
+    if (root == NULL) {
+        puts("Not Found!");
+        return NULL;
+    }
+
+    //Case 2: kalos Search ketemu
+    else if (root->id == id) {
+        puts("Found!");
+        return root;
+    }
+
+    // Case 3: kalo ID yang mau disearch lebih besar dari current id
+    else if (id > root->id) {
+        return search(root->right, id);
+    }
+
+    // Case 4: kalo ID yang mau disearch lebih kecil dari current id
+    else if (id < root->id) {
+        return search(root->left, id);
+    }
 }
 
 int main() {
@@ -57,6 +83,12 @@ int main() {
     // Traversal
     puts("Inorder Traversal: ");
     inorder(root);
+
+    // Searching
+    struct Student *searchedStudent = search(root, 378);
+    if (searchedStudent != NULL) {
+        printf("Found Student with id %d and name %s\n", searchedStudent->id, searchedStudent->name);
+    }
 
     return 0;
 }
