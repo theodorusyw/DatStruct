643ca71d69fe861cd1048aa1bb4a9a3420e5031d
theodorusyw <theo.yuriputra19@gmail.com>
2025-02-21 13:36:55 +0700

    #

--- Lab/test.cpp
+++ Lab/test.cpp
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+#define HASH_SIZE 100
+
+struct Booking {
+    char fullname[35];
+    char phonenumber[20];
+    int age;
+    char roomtype[10];
+    int stayduration;
+    char bookingid[7];
+    struct Booking* next;
+}*bookings[HASH_SIZE];
+
 int main() {
-    
+
 }
