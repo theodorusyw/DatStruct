d226a84220621e2021879c39dcc1adb9df99a08a
theodorusyw <theo.yuriputra19@gmail.com>
2025-04-25 13:38:39 +0700

    #

---
+++ Lab/heap.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+#define MAX_HEAP_SIZE 100
+
+int heap[MAX_HEAP_SIZE];
+int heapSize = 0;
+
+int left(int n) {
+    return n * 2 + 1;
+}
+
+int right(int n) {
+    return n * 2 + 2;
+}
+
+int parent(int n) {
+    return (n - 1) / 2;
+}
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void insertHeap(int val) {
+    if (heapSize == MAX_HEAP_SIZE) {
+        printf("Heap penuh\n");
+        return;
+    }
+    
+    int n = heapSize++;
+
+    heap[n] = val;
+
+    while (n != 0 && heap[parent(n)] < heap[n]) {
+        swap(&heap[parent(n)], &heap[n]);
+        n = parent(n);
+    }
+}
+
+void printHeapArray() {
+    for (int i = 0; i < heapSize; i++) {
+        printf("%d ", heap[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    return 0;
+}
