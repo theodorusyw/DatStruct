e64e28f8584239f3b69283dea4bb5471d22fc6de
theodorusyw <theo.yuriputra19@gmail.com>
2025-05-20 16:17:32 +0700

    #

--- Practice/QUIZ/ITHospital.cpp
+++ Practice/QUIZ/ITHospital.cpp
@@ -36,24 +36,36 @@ Node* rotateRight(Node* y) {
     x->right = y;
     y->left = T2;
 
-    y->height = max(height(y->left), height(y->right)) + 1;
     x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
 
     return x;
 }
 
 // rotasi ke kiri
-Node* leftRotate(Node* y) {
-    Node* x = y->right;
-    Node* T2 = x->left;
+Node* leftRotate(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
 
-    x->left = y;
-    y->right = T2;
+    y->left = y;
+    x->right = T2;
 
-    y->height = max(height(y->left), height(y->right)) + 1;
     x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
 
-    return x;
+    return y;
+}
+
+// membuat node baru
+Node* createNode(int id, char* name, int age, char* status) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->id = id;
+    strcpy(newNode->name, name);
+    newNode->age = age;
+    strcpy(newNode->status, status);
+    newNode->height = 1;
+    newNode->left = newNode->right = NULL;
+    return newNode;
 }
 
 int main() {
